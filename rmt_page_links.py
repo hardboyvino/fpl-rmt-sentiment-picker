cc9b0f2b44218ba29d635d27f305389e0fec1c40
Babaneyo <97181625+hardboyvino@users.noreply.github.com>
2023-02-10 19:21:23 +0100

    Add GW23 rmt page links

---
+++ rmt_page_links.py
@@ -0,0 +1,18 @@
+"""
+The links for all the Rate My Team pages for the respective GWs.
+
+Links are updated daily (hopefully), but as often as possible.
+
+All links attached to the GW are the pages for the days before the GW, after the last deadline 
+
+i.e. GW23 links are all the pages after GW 22 but before GW23 so usually the deadline day links are not included but is not a hard non-inclusion rule.
+"""
+
+gw23 = [
+    "https://www.reddit.com/r/FantasyPL/comments/10ua7vx/rate_my_team_quick_questions_general_advice_daily/",
+    "https://www.reddit.com/r/FantasyPL/comments/10v43p1/rate_my_team_quick_questions_general_advice_daily/",
+    "https://www.reddit.com/r/FantasyPL/comments/10vzcq1/rate_my_team_quick_questions_general_advice_daily/",
+    "https://www.reddit.com/r/FantasyPL/comments/10wu2cv/rate_my_team_quick_questions_general_advice_daily/",
+    "https://www.reddit.com/r/FantasyPL/comments/10xs9vw/rate_my_team_quick_questions_general_advice_daily/",
+    "https://www.reddit.com/r/FantasyPL/comments/10ypeth/rate_my_team_quick_questions_general_advice_daily/",
+]
