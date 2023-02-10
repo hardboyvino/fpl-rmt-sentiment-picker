a076741011112fb0f95ee9f962efb2be9af10cd8
Babaneyo <97181625+hardboyvino@users.noreply.github.com>
2023-02-10 18:39:28 +0100

    Create program to scrape all EPL player names

---
+++ all_player_names.py
@@ -0,0 +1,27 @@
+"""
+Get all the EPL Player names from fplform.com.
+
+There is a helpful table that has all the player names as the first column.
+"""
+
+
+def get_all_player_names(driver):
+    """Scrape fplform for the name of all players in FPL."""
+    # go to this site because they have a cool table with all players and some data
+    driver.get("https://www.fplform.com/fpl-player-data")
+
+    html = driver.page_source
+
+    # get the first (only) table on the page
+    df = pd.read_html(html)
+    df = df[0]
+
+    # create a list with all the player names
+    players = (df.loc[:, "Name"]).tolist()
+
+    # create a dictionary with all football players and initial count of [], using the player names list
+    all_players = {}
+    for player in players:
+        all_players[player] = 0
+
+    return all_players
