ce9cb9ad71e83907e4410e0d031f7f8c3b3eee6c
Babaneyo <97181625+hardboyvino@users.noreply.github.com>
2023-03-13 20:05:40 +0100

    Add page ids for GW28 RMT pages

--- rmt_page_links.py
+++ rmt_page_links.py
@@ -23,4 +23,6 @@ post_ids_25 = ["11681mt", "11744ho", "11811vd", "118vkjb", "119v8ex", "11aoztx"]
 
 post_ids_26 = ["11cdll5", "11d9e69", "11e3j1s", "11f154g", "11g08ex", "11gy464"]
 
-post_ids_27 = ["11iuov2", "11jw0vx", "11kwil7", "11ltorb"]
+post_ids_27 = ["11iuov2", "11jw0vx", "11kwil7", "11ltorb", "11mpl9r", "11nmbqm"]
+
+post_ids_28 = ["11pc49b", "11q6uq8"]
