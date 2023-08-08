394156728a5851d4500991679f33f7efecc0871e
Babaneyo <97181625+hardboyvino@users.noreply.github.com>
2023-08-08 23:31:52 +0100

    Rebuild the main program for collating the FPL sentiment data

--- main.py
+++ main.py
@@ -1,50 +1,66 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from collections import Counter
+from chromedriver_py import binary_path
 import pandas as pd
-import re
+from player_name_variations import player_name_variations
+from all_player_names import get_all_player_names, get_all_players_information
+from utils import get_all_comments_and_replies, merge_dfs, wildcard_team, count_rmt_word_occurence, count_player_fpl_occurence, add_new_players_to_variation
+from rmt_page_links import preseason
 
-from player_name_variations import player_name_vars
-from all_player_names import get_all_player_names
-from utils import count_player_occurence, get_all_comments_and_replies, merge_dfs, wildcard_team
-from rmt_page_links import post_ids_37
+# File Paths and Constants
+rmt_word_count_file = "rmt_word_counts.csv"
+player_mentions_csv = "players_mentions.csv"
+player_name_variations_file = "player_name_variations.csv"
+player_details_csv = "player_details.csv"
+reddit_comments_file = "reddit_comments.txt"
+optimized_team_file = "optimized_team.txt"
+BUDGET = 83.0
+files = [rmt_word_count_file, player_mentions_csv, reddit_comments_file, optimized_team_file]
 
-rmt_page_text = "rmt.txt"
-player_count_csv = "players.csv"
-file1 = "players.csv"
-file2 = "merged.csv"
-BUDGET = 83.3
-
-files = ["rmt.csv", player_count_csv, file1, "comments.txt", "optimized_team.txt"]
-
-# open all the web addresses in headless mode so it does not open a browser tab for the program
+# Set Up Selenium WebDriver
 options = Options()
 options.headless = True
 options.add_argument("--window-size=1920,1200")
 
-serve = Service(r"link to chromedriver")
-driver = webdriver.Chrome(options=options, service=serve)
+service = Service(binary_path)
+driver = webdriver.Chrome(options=options, service=service)
 
-for f in files:
-    # open a new rmt.txt file and if it exists, wipe it clean; then close it
-    new_file = open(f, "w")
+# Clear Contents of Files
+for file in files:
+    new_file = open(file, "w")
     new_file.close()
 
+# Get Player Information
+html = get_all_players_information(driver)
+player_mention_counts = get_all_player_names(html)
 
-all_players = get_all_player_names(driver, pd)
+# Load Player Name Variations
+player_name_variations_dict = player_name_variations(player_name_variations_file)
 
-# import all the various player name variations
-player_var_names = player_name_vars()
+# Add New Players to Variations
+player_name_variations_dict = add_new_players_to_variation(player_mention_counts, player_name_variations_dict)
+player_name_variations_dict = {k: v for k, v in sorted(player_name_variations_dict.items(), key=lambda item: item[0], reverse=False)}
 
-# get all words
-all_words = get_all_comments_and_replies(post_ids_37, re)
+# Update Player Variation File
+player_name_variations_df = pd.Series(player_name_variations_dict)
+player_name_variations_df.to_csv(player_name_variations_file, header=None)
 
-# count the number of times a player name was mentioned
-count_player_occurence(all_players, all_words, Counter, pd, player_count_csv, player_var_names)
+# Load Comments and Replies
+rmt_words_list = get_all_comments_and_replies(preseason, reddit_comments_file)
 
-# get wildcard team
-df_merged = merge_dfs(file1, file2, pd)
-wildcard_team(BUDGET, df_merged)
+# Count Word Occurrences in Comments
+word_counts_df = count_rmt_word_occurence(rmt_words_list, rmt_word_count_file)
+word_counts_df.pop('')
 
-df_merged.to_csv("finals.csv", index=False)
+# Count Player FPL Mentions
+count_player_fpl_occurence(player_mention_counts, player_mentions_csv, player_name_variations_dict, word_counts_df)
+
+# Merge DataFrames
+merged_player_details_df = merge_dfs(player_mentions_csv, player_details_csv)
+
+# Create Wildcard Team
+wildcard_team(BUDGET, merged_player_details_df)
+
+# Save Final DataFrame
+merged_player_details_df.to_csv("complete_player_info.csv", index=False)
